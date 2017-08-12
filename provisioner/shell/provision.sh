2ea98ef90ca56705ec1a9c73679eb76f6961b5d3
Greg Work <greg.work@gmail.com>
2017-08-12 13:52:06 +0930

    Shell provisioner: fix redirect to stderr, not file '2'

--- provisioner/shell/provision.sh
+++ provisioner/shell/provision.sh
@@ -5,7 +5,7 @@ set -o nounset
 
 function check_user {
   if [[ ${EUID} -ne 0 ]]; then
-    >2 echo "This script requires root; please re-execute as root or under sudo"
+    >&2 echo "This script requires root; please re-execute as root or under sudo"
     exit 1
   fi
 }
@@ -14,7 +14,7 @@ function install_package {
   local PACKAGE=$1
 
   if ! rpm -q ${PACKAGE} &>/dev/null ; then
-    >2 echo "Package ${PACKAGE} not installed; installing..."
+    >&2 echo "Package ${PACKAGE} not installed; installing..."
     yum --assumeyes install ${PACKAGE}
   fi
 }
@@ -23,12 +23,12 @@ function start_service {
   local SERVICE=$1
 
   if ! systemctl is-enabled ${SERVICE} &>/dev/null ; then
-    >2 echo "Service ${SERVICE} not enabled; enabling..."
+    >&2 echo "Service ${SERVICE} not enabled; enabling..."
     systemctl enable ${SERVICE}
   fi
 
   if ! systemctl is-active ${SERVICE} &>/dev/null ; then
-    >2 echo "Service ${SERVICE} not active; starting..."
+    >&2 echo "Service ${SERVICE} not active; starting..."
     systemctl start ${SERVICE}
   fi
 }
@@ -40,14 +40,14 @@ function set_system_timezone {
   EXPECTED_TZFILE="/usr/share/zoneinfo/${TIMEZONE}"
 
   if [ ! -e ${EXPECTED_TZFILE} ] ; then
-    >2 echo "No timezone file for ${TIMEZONE} found; exiting..."
+    >&2 echo "No timezone file for ${TIMEZONE} found; exiting..."
     exit 1
   fi
 
   ACTIVE_TZFILE=$(readlink -f /etc/localtime)
 
   if [ "${ACTIVE_TZFILE}" != "${EXPECTED_TZFILE}" ] ; then
-    >2 echo "System TZ is not set to ${TIMEZONE}; setting..."
+    >&2 echo "System TZ is not set to ${TIMEZONE}; setting..."
     timedatectl set-timezone ${TIMEZONE}
   fi
 }
@@ -58,7 +58,7 @@ function set_php_timezone {
   CURRENT_TZ=$(php -i | grep "^date.timezone")
 
   if [[ ! "${CURRENT_TZ}" =~ "${TIMEZONE}" ]]; then
-    >2 echo "PHP TZ is not set to ${TIMEZONE}; setting..."
+    >&2 echo "PHP TZ is not set to ${TIMEZONE}; setting..."
     echo "date.timezone=${TIMEZONE}" > /etc/php.d/zz-timezone.ini
   fi
 }
@@ -74,7 +74,7 @@ function create_phpinfo {
   local FILE="/var/www/html/phpinfo.php"
 
   if [ ! -e "${FILE}" ] ; then
-    >2 echo "phpinfo() not found; creating..."
+    >&2 echo "phpinfo() not found; creating..."
 
     cat > "${FILE}" <<EOF
     <?php phpinfo(); ?>
@@ -86,7 +86,7 @@ function open_firewall_port {
   local SERVICE=$1
 
   if ! firewall-cmd --zone=public --query-service=${SERVICE} ; then
-    >2 echo "Firewall port for ${SERVICE} not open; adding rule..."
+    >&2 echo "Firewall port for ${SERVICE} not open; adding rule..."
     firewall-cmd --permanent --zone=public --add-service=http
     firewall-cmd --reload
   fi
